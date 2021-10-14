fe67dfe66b203b02ac68f0a3ec360b15bc165935
techwulf <t.zach.moore@gmail.com>
2021-10-14 15:59:57 -0600

    implement get request /api/energies

--- api/energies/energies-router.js
+++ api/energies/energies-router.js
@@ -1,3 +1,15 @@
 const router = require('express').Router();
 
+const Energies = require('./energies-model');
+
+router.get('/', (req, res, next) => {
+  Energies.getAll()
+    .then(energies => {
+      res.status(200).json(energies);
+    })
+    .catch(error => {
+      next(error);
+    });
+});
+
 module.exports = router;
--- api/server.test.js
+++ api/server.test.js
@@ -22,7 +22,18 @@ describe('[GET] /api/energies', () => {
     res = await request(server).get('/api/energies');
   });
 
-  it.todo('responds with 200 OK');
-  it.todo('responds with 4 energies');
-  it.todo('responds with correct structure');
+  it('responds with 200 OK', async () => {
+    expect(res.status).toBe(200);
+  });
+  it('responds with 4 energies', async () => {
+    expect(res.body).toHaveLength(4);
+  });
+  it('responds with correct structure', () => {
+    expect(res.body).toMatchObject([
+      {id: 1, energy: "demonic"},
+      {id: 2, energy: "fey"},
+      {id: 3, energy: "celestial"},
+      {id: 4, energy: "undead"}
+    ]);
+  });
 });
