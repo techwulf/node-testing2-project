0328449012c1cbef0c440e081ce1b2cc6b6207ad
techwulf <t.zach.moore@gmail.com>
2021-10-14 17:17:45 -0600

    implement put and delete request /api/energies/:id

--- api/energies/energies-router.js
+++ api/energies/energies-router.js
@@ -12,4 +12,34 @@ router.get('/', (req, res, next) => {
     });
 });
 
+router.post('/', (req, res, next) => {
+  Energies.insert(req.body)
+    .then(newEnergy => {
+      res.status(201).json(newEnergy);
+    })
+    .catch(err => {
+      next(err)
+    });
+});
+
+router.put('/:id', (req, res, next) => {
+  Energies.update(req.params.id, req.body.energy)
+    .then(updatedEnergy => {
+      res.status(200).json(updatedEnergy);
+    })
+    .catch(err => {
+      next(err);
+    });
+});
+
+router.delete('/:id', (req, res, next) => {
+  Energies.remove(req.params.id)
+    .then(removedEnergy => {
+      res.status(200).json(removedEnergy);
+    })
+    .catch(err => {
+      next(err);
+    });
+});
+
 module.exports = router;
--- api/server.test.js
+++ api/server.test.js
@@ -37,3 +37,70 @@ describe('[GET] /api/energies', () => {
     ]);
   });
 });
+
+describe('[POST] /api/energies', () => {
+  let res;
+  const newEnergy = {energy: 'animate'};
+  beforeEach(async () => {
+    res = await request(server).post('/api/energies')
+      .send(newEnergy);
+  });
+
+  it('responds with 201 OK', async () => {
+    expect(res.status).toBe(201);
+  });
+
+  it('causes an energy to be added to the db', async () => {
+    const energies = await db('energies');
+    expect(energies).toHaveLength(5);
+  });
+
+  it('responds with the newly created energy', async () => {
+    expect(res.body).toMatchSnapshot();
+  });
+});
+
+describe('[PUT] /api/energies/:id', () => {
+  let res;
+  const id = 3;
+  const updatedEnergy = {energy: 'cosmic'};
+  beforeEach(async () => {
+    res = await request(server).put(`/api/energies/${id}`)
+      .send(updatedEnergy);
+  });
+
+  it('responds with 200 OK', async () => {
+    expect(res.status).toBe(200);
+  });
+
+  it('responds with the newly updated energy', async () => {
+    expect(res.body).toMatchObject({id: 3, energy: 'cosmic'});
+  });
+
+  it('causes update to show up in database', async () => {
+    const energy = await db('energies').where('id', id).first();
+    expect(energy).toMatchObject({id: 3, energy: 'cosmic'});
+  });
+});
+
+describe('[DELETE] /api/energies/:id', () => {
+  let res;
+  const id = 4;
+  beforeEach(async () => {
+    res = await request(server).delete(`/api/energies/${id}`)
+  });
+
+  it('responds with 200 OK', async () => {
+    expect(res.status).toBe(200);
+  });
+
+  it('responds with the deleted energy', async () => {
+    expect(res.body).toMatchObject({id: 4, energy: 'undead'});
+  });
+
+  it('causes energy to delete from the server', async () => {
+    const energies = await db('energies');
+    expect(energies).toHaveLength(3);
+  });
+
+})
